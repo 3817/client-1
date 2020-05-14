5e9e8a7afbf808810834e142855ebbb116bbd110
ollie <64110522+ollieobama@users.noreply.github.com>
2020-05-14 10:01:53 +1000

    Added DonkeyFind

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/DonkeyFind.java
@@ -0,0 +1,51 @@
+package me.zeroeightsix.kami.module.modules.misc;
+
+import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityDonkey;
+import net.minecraft.entity.passive.EntityLlama;
+import net.minecraft.util.math.BlockPos;
+import scala.Int;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import static me.zeroeightsix.kami.util.MessageSendHelper.sendChatMessage;
+
+@Module.Info(name = "Donkey Finder", category = Module.Category.MISC, description = "Finds you a Donkey!")
+public class DonkeyFind extends Module {
+
+    private List<String> knownPlayers;
+    boolean test = false;
+
+    @Override
+    public void onUpdate() {
+        if (mc.player == null) return;
+
+        List<Integer> tickPlayerList = new ArrayList<>();
+
+        for (Entity entity : mc.world.getLoadedEntityList()) {
+            if (entity instanceof EntityDonkey) {
+                sendChatMessage("I found a donkey at: " + Math.round(entity.lastTickPosX) + " " + Math.round(entity.lastTickPosY) + " " + Math.round(entity.lastTickPosZ));
+            }
+
+
+            if (tickPlayerList.size() > 0) {
+
+
+            }
+        }
+    }
+
+
+            @Override
+            public void onEnable () {
+                this.knownPlayers = new ArrayList<>();
+
+            }
+
+        }
